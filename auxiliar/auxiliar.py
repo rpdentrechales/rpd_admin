a2205c3904fccd206592912fd741c111168f7250
rpdentrechales <rpdprocorpo@gmail.com>
2025-01-10 15:35:27 +0000

    Updating files

--- auxiliar/auxiliar.py
+++ auxiliar/auxiliar.py
@@ -22,6 +22,34 @@ def get_dataframe_from_mongodb(collection_name, database_name, query={}):
 
     return dataframe
 
+def upload_dataframe_to_mongodb(collection_name,database_name, dataframe, unique_key):
+  print(f"uploading data to {database_name} : {collection_name}")
+  client = MongoClient(f"mongodb+srv://rpdprocorpo:iyiawsSCfCsuAzOb@cluster0.lu6ce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  db = client[database_name]
+  collection = db[collection_name]
+
+  bulk_operations = []
+  for _, row in dataframe.iterrows():
+      item = row.to_dict()
+      query = {unique_key: item[unique_key]}
+      update = {"$set": item}
+      bulk_operations.append(UpdateOne(query, update, upsert=True))
+
+  if bulk_operations:
+      result = collection.bulk_write(bulk_operations)
+      results = {
+          "inserted": result.upserted_count,
+          "updated": result.modified_count,
+          "matched": result.matched_count,
+      }
+  else:
+      results = {"inserted": 0, "updated": 0, "matched": 0}
+
+  print(f"upload results: {database_name} : {collection_name} - {results}")
+
+  return results
+
+
 def plot_sales_count(df):
     df['date'] = pd.to_datetime(df['date'])
     df['formatted_date'] = df['date'].dt.strftime('%d/%m/%Y')
--- views/configurar_vendedoras.py
+++ views/configurar_vendedoras.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import datetime
 from auxiliar.auxiliar import *
+import hashlib
+
+def convert_name_to_id(name):
+  id = hashlib.md5(name.encode()).hexdigest()[:12]
+  return id
 
 st.set_page_config(page_title="RPD - Configurar Vendedoras", page_icon="💎",layout="wide")
 
@@ -9,4 +14,5 @@ st.title("Dados das Vendedoras")
 
 vendedoras_df = get_dataframe_from_mongodb(collection_name="dados_vendedoras", database_name="rpd_db")
 
+st.write(vendedoras_df.columns)
 st.dataframe(vendedoras_df,hide_index=True,use_container_width=True)
